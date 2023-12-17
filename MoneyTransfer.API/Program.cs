18b69b6ff1a54c4b41636f7b8285e1d1e9e43fde
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-16 22:07:14 -0500

    added connection string and registered db context in api project program.cs

--- MoneyTransfer.API/Program.cs
+++ MoneyTransfer.API/Program.cs
@@ -1,4 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MoneyTransfer.API.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .Build();
+
+var connectionString = config.GetConnectionString("Project");
+
+builder.Services.AddDbContext<MoneyTransferContext>(options => 
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
