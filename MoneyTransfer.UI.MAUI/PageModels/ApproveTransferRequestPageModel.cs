142de7b880253fc7f0a3828f8600b65e729629f1
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-20 19:52:32 -0500

    completed 'approve transfer request' and 'reject transfer request' pages in MAUI project

--- MoneyTransfer.UI.MAUI/PageModels/ApproveTransferRequestPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/ApproveTransferRequestPageModel.cs
@@ -1,8 +1,45 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTransfer.UI.MAUI.Services;
+using MoneyTransfer.UI.MAUI.Services.Models;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class ApproveTransferRequestPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class ApproveTransferRequestPageModel : ObservableObject
     {
+        private readonly IDataService _dataService;
+
+        public ApproveTransferRequestPageModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            LoadData();
+        }
+
+        [ObservableProperty]
+        private TransferDetails _transferDetails = default!;
+
+        [RelayCommand]
+        private void Approve()
+        {
+            if (!CanApprove) { return; }
+        }
+
+        [RelayCommand]
+        private void Cancel()
+        {
+            if (!CanCancel) { return; }
+        }
+
+        [ObservableProperty]
+        private bool canApprove = true; // TODO: if type == request && status == pending && user from is the current logged in user
+
+        [ObservableProperty]
+        private bool canCancel = true;
+
+        private async void LoadData()
+        {
+            // TODO: The passed in id is hard coded here for testing
+            TransferDetails = await _dataService.GetTransferDetailsAsync(1) ?? Helpers.TransferNotFound;
+        }
     }
 }
--- MoneyTransfer.UI.MAUI/PageModels/RejectTransferRequestPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/RejectTransferRequestPageModel.cs
@@ -1,8 +1,45 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTransfer.UI.MAUI.Services;
+using MoneyTransfer.UI.MAUI.Services.Models;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class RejectTransferRequestPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class RejectTransferRequestPageModel : ObservableObject
     {
+        private readonly IDataService _dataService;
+
+        public RejectTransferRequestPageModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            LoadData();
+        }
+
+        [ObservableProperty]
+        private TransferDetails _transferDetails = default!;
+
+        [RelayCommand]
+        private void Reject()
+        {
+            if (!CanReject) { return; }
+        }
+
+        [RelayCommand]
+        private void Cancel()
+        {
+            if (!CanCancel) { return; }
+        }
+
+        [ObservableProperty]
+        private bool canReject = true; // TODO: if type == request && status == pending && user from is the current logged in user
+
+        [ObservableProperty]
+        private bool canCancel = true;
+
+        private async void LoadData()
+        {
+            // TODO: The passed in id is hard coded here for testing
+            TransferDetails = await _dataService.GetTransferDetailsAsync(1) ?? Helpers.TransferNotFound;
+        }
     }
 }
