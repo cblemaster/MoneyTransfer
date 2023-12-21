d56dd3d3a8fb68ac78518a7656e0b11e203c58f9
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-21 15:00:06 -0500

    added page routes to appshell in MAUI project

--- MoneyTransfer.UI.MAUI/AppShell.xaml.cs
+++ MoneyTransfer.UI.MAUI/AppShell.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace MoneyTransfer.UI.MAUI
+﻿
+using MoneyTransfer.UI.MAUI.Pages;
+
+namespace MoneyTransfer.UI.MAUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterPageRoutes();
+        }
+
+        private void RegisterPageRoutes()
+        {
+            Routing.RegisterRoute("ApproveTransfer", typeof(ApproveTransferRequestPage));
+            Routing.RegisterRoute("RejectTransfer", typeof(RejectTransferRequestPage));
+            Routing.RegisterRoute("TransferDetails", typeof(TransferDetailsPage));
         }
     }
 }
