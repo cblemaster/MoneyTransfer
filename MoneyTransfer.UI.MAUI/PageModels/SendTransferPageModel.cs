dcd11b4f8afbab62b268bd9ee070d7987788380e
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-23 20:52:29 -0500

    completed 'send transfer' in MAUI project

--- MoneyTransfer.UI.MAUI/PageModels/SendTransferPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/SendTransferPageModel.cs
@@ -1,8 +1,70 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTransfer.UI.MAUI.Services;
+using System.Collections.ObjectModel;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class SendTransferPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class SendTransferPageModel(IDataService dataService, IMockUserService mockUserService) : ObservableObject
     {
+        private readonly IDataService _dataService = dataService;
+        private readonly IMockUserService _mockUserService = mockUserService;
+
+        [ObservableProperty]
+        private ReadOnlyCollection<User> _users = default!;
+
+        [ObservableProperty]
+        private User _selectedUser = default!;
+
+        [ObservableProperty]
+        private string _amount = default!;
+
+        [RelayCommand]
+        private void PageAppearing() => LoadData();
+
+        [RelayCommand]
+        private async Task SendTransfer()
+        {
+            if (!CanSendTransfer) { return; }
+            if (SelectedUser is null)
+            {
+                CanSendTransfer = false;
+                return;
+            }
+            if (!decimal.TryParse(Amount, out decimal amount) || amount <= 0)
+            {
+                CanSendTransfer = false;
+                return;
+            }
+
+            User UserFrom = (await _mockUserService.GetLoggedInUserAsync())!;
+            User UserTo = (await _mockUserService.GetUserById(SelectedUser.Id))!;
+
+            decimal userFromBalance = (await _dataService.GetAccountDetailsForUserAsync(UserFrom.Id)).CurrentBalance;
+            if (userFromBalance < amount)
+            {
+                await Shell.Current.DisplayAlert("Error!", "Only transfer requests with an amount less than or equal to your current balance can be sent.", "OK");
+                CanSendTransfer = false;
+                return;
+            }
+
+            if (UserFrom.Id == UserTo.Id)
+            {
+                CanSendTransfer = false;
+                return;
+            }
+
+            await _dataService.SendTransferAsync(UserFrom.Username, UserTo.Username, amount);
+            await Shell.Current.DisplayAlert("Success!", "Transfer sent, go to Completed Transfers to see it.", "OK");
+        }
+
+        [ObservableProperty]
+        private bool _canSendTransfer = true;
+
+        private async void LoadData()
+        {
+            User loggedInUser = (await _mockUserService.GetLoggedInUserAsync())!;
+            Users = (await _mockUserService.AllUsersNotLoggedIn(loggedInUser))!;
+        }
     }
 }
