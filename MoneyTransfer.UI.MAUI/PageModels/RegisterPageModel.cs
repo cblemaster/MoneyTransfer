7b1f8774fa82f68fe3bf1cf64cd2c558b9829375
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-23 22:43:36 -0500

    added skeleton for 'register' in MAUI project

--- MoneyTransfer.UI.MAUI/PageModels/RegisterPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/RegisterPageModel.cs
@@ -1,8 +1,26 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTransfer.UI.MAUI.Services;
+using System.Security;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class RegisterPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class RegisterPageModel() : ObservableObject
     {
+        [ObservableProperty]
+        private string _username = default!;
+
+        [ObservableProperty]
+        private string _password = default!;
+
+        [RelayCommand]
+        private void Register()
+        {
+            if (!CanRegister) { return; }
+            // TODO: Logic for registering a new user
+        }
+
+        [ObservableProperty]
+        private bool _canRegister = true;
     }
 }
