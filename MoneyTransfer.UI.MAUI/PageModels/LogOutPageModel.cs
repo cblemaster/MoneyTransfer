ccccb6c5bfe0eadda8118795a9f7e30f48a783ec
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-23 21:30:23 -0500

    added skeleton for 'log out' in MAUI project

--- MoneyTransfer.UI.MAUI/PageModels/LogOutPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/LogOutPageModel.cs
@@ -1,8 +1,15 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class LogOutPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class LogOutPageModel() : ObservableObject
     {
+        [RelayCommand]
+        private async Task LogOut()
+        {
+            //TODO: Logic to log out current logged in user
+            await Shell.Current.GoToAsync("///LogIn");
+        }
     }
 }
