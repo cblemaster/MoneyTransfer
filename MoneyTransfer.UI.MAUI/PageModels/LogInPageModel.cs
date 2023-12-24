32fada3e13387253feb76181704f3889bec9f18b
Brian LeMaster <brian.lemaster@outlook.com>
2023-12-23 23:00:08 -0500

    added skeleton for 'log in' in MAUI project

--- MoneyTransfer.UI.MAUI/PageModels/LogInPageModel.cs
+++ MoneyTransfer.UI.MAUI/PageModels/LogInPageModel.cs
@@ -1,8 +1,25 @@
-﻿using MoneyTransfer.UI.MAUI.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyTransfer.UI.MAUI.Services;
 
 namespace MoneyTransfer.UI.MAUI.PageModels
 {
-    public class LogInPageModel(IDataService dataService) : PageModelBase(dataService)
+    public partial class LogInPageModel() : ObservableObject
     {
+        [ObservableProperty]
+        private string _username = default!;
+
+        [ObservableProperty]
+        private string _password = default!;
+
+        [RelayCommand]
+        private void LogIn()
+        {
+            if (!CanLogIn) { return; }
+            // TODO: Logic for logging in a user
+        }
+
+        [ObservableProperty]
+        private bool _canLogIn = true;
     }
 }
