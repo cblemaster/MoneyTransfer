e73d43ea0d726001e4da2bd0be663b1404e77674
Brian LeMaster <brian.lemaster@outlook.com>
2024-03-16 17:29:43 -0400

    added validationresult class

---
+++ MoneyTransfer.Core/Validation/ValidationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace MoneyTransfer.Core.Validation;
+
+public class ValidationResult
+{
+    public required bool IsValid { get; init; }
+
+    public required string ErrorMessage { get; init; }
+}
